334c79e63997317d5397b8bae15a3b772c799dc8
Michal Kmiec <michal.kmiec01@gmail.com>
2022-12-16 19:10:47 +0100

    Add project files.

---
+++ FibrusWPF/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using FibrusWPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FibrusWPF
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override async void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<ShellViewModel>();
+        }
+    }
+}
---
+++ FibrusWPF/ViewModels/ShellViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibrusWPF.ViewModels
+{
+    public class ShellViewModel : Screen
+    {
+		private string? _firstName;
+
+		public string FirstName
+		{
+			get 
+			{
+				return _firstName; 
+			}
+			set 
+			{ 
+				_firstName = value; 
+			}
+		}
+
+	}
+}
